9d1b45d91550a84ab2b4d897f0441c93b8e43dc6
Amore Bartsch <amorebartsch@dhcp-128-189-229-60.ubcsecure.wireless.ubc.ca>
2015-09-28 13:18:52 -0700

    first commit

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -66,7 +66,7 @@ public class GitLab implements EntryPoint {
 
 			@Override
 			public void onSuccess(List<IUser> result) {
-				Window.alert("Got list back with " +  result.size() + " entries");
+				displayUsers(result);
 				
 			}});
 	
